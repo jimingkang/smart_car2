a313ff684241e39d15a957f0c3c06dbbcae0ebd6
jimingkang <jimingkang@gmail.com>
2019-04-16 19:47:34 -0400

    merge int with str

--- myFile.py
+++ myFile.py
@@ -1,19 +1,6 @@
 import os
-def test():
-    newfile = "./homeposition.txt"
-    if not os.path.exists(newfile):
-        f = open(newfile,'w')
-        print newfile
-        f.close()
-        print newfile + " created."
-    else:
-        print newfile + " already existed."
-        fo = open(newfile, 'r+')
-        line=fo.readline()
-        line = line.strip()
-        homeposition=line
-        print "homeposition ", (homeposition)
-        fo.close()
-    return
 
-test()
+
+a=12
+bb=str(12)
+print bb+" bb"
