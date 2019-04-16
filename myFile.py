c29e9539bb884469303a1ccf1c80d5793bbd5b3b
jimingkang <jimingkang@gmail.com>
2019-04-16 19:55:07 -0400

    restore to home position in setup

--- myFile.py
+++ myFile.py
@@ -1,6 +1,21 @@
 import os
+def test():
+    newfile = "./homeposition.txt"
+    if not os.path.exists(newfile):
+        f = open(newfile,'w')
+        print newfile
+        f.close()
+        print newfile + " created."
+    else:
+        print newfile + " already existed."
+        fo = open(newfile, 'r+')
+        line=fo.readline()
+        line = line.strip()
+        homeposition=line
+        print "homeposition ", (homeposition)
+        ss=homeposition.split(' ', 1);
+        print ss[0]
+        fo.close()
+    return
 
-
-a=12
-bb=str(12)
-print bb+" bb"
+test()
